bef004d126764449c1dce45888394ced985c1fc6
Matheus Silveira <theunewba@gmail.com>
2025-06-19 01:58:23 -0300

    criando model de restaurante

---
+++ src/components/RestaurantCard/index.tsx
@@ -0,0 +1,33 @@
+import estrela from '../../assets/images/estrela.png'
+import Tag from '../Tag'
+import { Card, Description, Infos, Rating, Title } from './styles'
+
+type Props = {
+  image: string
+  title: string
+  rating: string
+  category: string[]
+  description: string
+}
+
+const Restaurant = ({ image, title, rating, category, description }: Props) => (
+  <Card>
+    <img src={image} alt={title} />
+    <Infos>
+      {category.map((category) => (
+        <Tag key={category}>{category}</Tag>
+      ))}
+    </Infos>
+    <div>
+      <Title>Hioki Sushi</Title>
+      <Rating>
+        <span>{rating}</span>
+        <img src={estrela} alt="Estrela" />
+      </Rating>
+    </div>
+    <Description>{description}</Description>
+    <button>Saiba mais</button>
+  </Card>
+)
+
+export default Restaurant
